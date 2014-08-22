6863ac74a2f2e9ba4bfd8f9c6bffb8e7610754b0
Mark Centoni <marklc44@gmail.com>
2014-08-22 09:11:39 -0700

    fix comment#update redirect

--- rails_blog_lab/app/controllers/comments_controller.rb
+++ rails_blog_lab/app/controllers/comments_controller.rb
@@ -34,8 +34,7 @@ class CommentsController < ApplicationController
     comment = params.require(:comment).permit(:author, :body, :title)
     @comment = @post.comments.find_by_id(comment_id).update_attributes(comment)
 
-    # this is all fucked up
-    redirect_to [@post]
+    redirect_to @post
   end
 
   def destroy
@@ -52,6 +51,7 @@ class CommentsController < ApplicationController
     post_id = params[:post_id]
     @post = Post.find_by_id(post_id)
   end
+
   def find_comment_by_id
     comment_id = params[:id]
     @comment = @post.comments.find_by_id(comment_id)
